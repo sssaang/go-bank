d3bbe0f9ad99f2d5c7bcae4d43fb22b062454942
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-03 00:21:34 +0900

    feat: add temp transaction for transfers between accounts

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,76 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sssaang/simplebank/db/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTranferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	n := 5
+	amount := util.RandomInt(0, account1.Balance)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID: account2.ID,
+				Amount: amount,
+			})
+
+			errs <-err
+			results <-result
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, transfer.FromAccountID, account1.ID)
+		require.Equal(t, transfer.ToAccountID, account2.ID)
+		require.Equal(t, transfer.Amount, amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, fromEntry.AccountID, account1.ID)
+		require.Equal(t, fromEntry.Amount, -amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, toEntry.AccountID, account2.ID)
+		require.Equal(t, toEntry.Amount, amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err) 
+
+		// require.Equal(t, result.FromAccount.ID, account1.ID)
+		// require.Equal(t, result.ToAccount.ID, account2.ID)
+		// require.Equal(t, result.ToEntry.AccountID, account2.ID)
+	}
+
+}
