270226146903c9ec56a5332768b9ee17bdcd8624
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-05 21:12:33 +0900

    feat: add transaction unit test with inconsistent order

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -106,3 +106,54 @@ func TestFiveConcurrentTranferTx(t *testing.T) {
 	require.Equal(t, updatedAccount1.Balance, account1.Balance - amount * int64(n))
 	require.Equal(t, updatedAccount2.Balance, account2.Balance + amount * int64(n))
 }
+
+func TestInconsistentOrderTranferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	t.Logf(">>>>> Before Transaction Account1: %d Account2: %d\n", account1.Balance, account2.Balance)
+
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i % 2 == 1 {
+			fromAccountID, toAccountID = toAccountID, fromAccountID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID: toAccountID,
+				Amount: amount,
+			})
+
+			errs <-err
+		}()
+	}
+ 
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+		// TODO Clean up for concurrent test
+		
+	}
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)	
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount1)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)	
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount2)
+
+	t.Logf(">>>>> After all %d Transactions Account1: %d Account2: %d\n", n, updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, updatedAccount1.Balance, account1.Balance)
+	require.Equal(t, updatedAccount2.Balance, account2.Balance)
+}
