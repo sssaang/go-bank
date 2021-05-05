ee6158d91a77b5c2f05a4efa4b87a4e566727625
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-06 01:35:07 +0900

    feat: add get account list api

--- api/account.go
+++ api/account.go
@@ -51,7 +51,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-		return 
+			return 
 		}
 
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
@@ -59,4 +59,30 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountsRequest struct {
+	PageID int32 `from:"page_id" binding:required,min=1"`
+	PageSize int32 `from:"page_size" binding:required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit: req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil { 
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return 
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
