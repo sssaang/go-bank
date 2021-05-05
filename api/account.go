b4e7ea4c791ac3398442e9d96e0fca2c09217b71
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-06 01:42:49 +0900

    fix: req parameter constraints

--- api/account.go
+++ api/account.go
@@ -37,7 +37,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 type getAccountRequest struct {
-	ID int64 `uri:"id" binding:required,min=1"`
+	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -62,8 +62,8 @@ func (server *Server) getAccount(ctx *gin.Context) {
 }
 
 type listAccountsRequest struct {
-	PageID int32 `from:"page_id" binding:required,min=1"`
-	PageSize int32 `from:"page_size" binding:required,min=5,max=10"`
+	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) listAccounts(ctx *gin.Context) {
