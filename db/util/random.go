da1f8af00faa6ec8195ad1e9242e4abb6eab6e25
Sang <thanato0912@gmail.com>
2021-05-19 21:24:39 +0900

    feat: add password hashing

--- db/util/random.go
+++ db/util/random.go
@@ -10,6 +10,15 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+func RandomString(num int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	 b := make([]rune, num)
+	for i := range b {
+			b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max - min + 1)
 }
--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/lib/pq v1.10.1
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 )
