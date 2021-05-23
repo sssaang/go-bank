d8efccf5f246b04bc79bb87649ff7518e9ba02bf
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-23 22:46:27 +0900

    feat: add expired token test case

--- token/jwt_manager_test.go
+++ token/jwt_manager_test.go
@@ -30,3 +30,18 @@ func TestJWTManager(t *testing.T) {
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 }
+
+func TestExpiredJWTToken(t *testing.T) {
+	manager, err := NewJWTManager(util.RandomString(32))
+	require.NoError(t, err)
+	
+	username := util.RandomEmail()
+	token, err := manager.CreateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := manager.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
