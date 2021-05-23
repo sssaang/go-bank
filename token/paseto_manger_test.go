e94e12333b516a0a539e08d27acd71f9a2d0ce82
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-23 23:54:23 +0900

    feat: add expired paseto token test case

--- token/paseto_manger_test.go
+++ token/paseto_manger_test.go
@@ -31,3 +31,37 @@ func TestPasetoManager(t *testing.T) {
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 }
+
+func TestExpiredPasetoToken(t *testing.T) {
+	manager, err := NewPasetoManager(util.RandomString(chacha20poly1305.KeySize))
+	require.NoError(t, err)
+	
+	username := util.RandomEmail()
+	token, err := manager.CreateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := manager.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+// TODO: add invalid paseto token test case
+
+// func TestInvalidPasetoToken(t *testing.T) {
+// 	manager, err := NewPasetoManager(util.RandomString(chacha20poly1305.KeySize))
+// 	require.NoError(t, err)
+	
+// 	username := util.RandomEmail()
+// 	duration := time.Minute
+
+// 	token, err := manager.CreateToken(username, duration)
+// 	require.NoError(t, err)
+// 	require.NotEmpty(t, token)
+
+// 	payload, err := manager.VerifyToken("fasdfdsa.fadsfdsfds.fdsafsd")
+// 	require.Error(t, err)
+// 	require.EqualError(t, err, ErrInvalidToken.Error())
+// 	require.Nil(t, payload)
+// }
