ec7bcf3878098c86b8d03c7f1f58e9e761d199b9
Sangmin  Kim <sssaang@Sangminui-MacBookPro.local>
2021-05-26 02:08:32 +0900

    feat: add auth middleware to verify access token

---
+++ api/middleware.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sssaang/simplebank/token"
+)
+
+const (
+	AUTHORIZATION_HEADER = "authorization"
+	AUTHORIZATION_TYPE_BEARER = "bearer"
+	AUTHORIZATION_PAYLOAD = "authorization_payload"
+)
+
+func authMiddleware(manager token.TokenManager) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authorizationHeader := ctx.GetHeader(AUTHORIZATION_HEADER)
+		if len(authorizationHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authorizationHeader)
+		if len(fields) < 2 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != AUTHORIZATION_TYPE_BEARER {
+			err := errors.New("unsupported authorization type")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := manager.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(AUTHORIZATION_PAYLOAD, payload)
+		ctx.Next()
+	}
+}
